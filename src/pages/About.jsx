cc66cdbc756f47593dafad6d2a6cffb199a8163e
Ankit Kumar <ankitkumarr0402@gmail.com>
2024-02-13 11:41:45 +0530

    page completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1>About us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sed hic sequi alias, consequatur architecto voluptas impedit voluptate repellendus facilis.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sed hic sequi alias, consequatur architecto voluptas impedit voluptate repellendus facilis.</p>
+    </div>
   )
 }
