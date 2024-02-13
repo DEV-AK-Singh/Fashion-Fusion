5754bad9cbe8785c01f32ce1fe29cdc718e9dde4
Abhishek Kumar Singh <singh.abhishek151019@gmail.com>
2024-02-13 11:42:00 +0530

    About page completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='container my-5'>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quasi placeat hic cum aliquid modi alias commodi debitis odit nisi quos dignissimos voluptas aut ex ipsum assumenda numquam, repudiandae rem laboriosam amet nemo temporibus! Velit cumque exercitationem excepturi accusamus veritatis! Corporis eveniet neque nemo eos. Beatae hic ex expedita repellendus tenetur nobis ad vitae, dicta quibusdam sed dolorum, modi natus?</p><br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quasi placeat hic cum aliquid modi alias commodi debitis odit nisi quos dignissimos voluptas aut ex ipsum assumenda numquam, repudiandae rem laboriosam amet nemo temporibus! Velit cumque exercitationem excepturi accusamus veritatis! Corporis eveniet neque nemo eos. Beatae hic ex expedita repellendus tenetur nobis ad vitae, dicta quibusdam sed dolorum, modi natus?</p><br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quasi placeat hic cum aliquid modi alias commodi debitis odit nisi quos dignissimos voluptas aut ex ipsum assumenda numquam, repudiandae rem laboriosam amet nemo temporibus! Velit cumque exercitationem excepturi accusamus veritatis! Corporis eveniet neque nemo eos. Beatae hic ex expedita repellendus tenetur nobis ad vitae, dicta quibusdam sed dolorum, modi natus?</p><br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quasi placeat hic cum aliquid modi alias commodi debitis odit nisi quos dignissimos voluptas aut ex ipsum assumenda numquam, repudiandae rem laboriosam amet nemo temporibus! Velit cumque exercitationem excepturi accusamus veritatis! Corporis eveniet neque nemo eos. Beatae hic ex expedita repellendus tenetur nobis ad vitae, dicta quibusdam sed dolorum, modi natus?</p><br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quasi placeat hic cum aliquid modi alias commodi debitis odit nisi quos dignissimos voluptas aut ex ipsum assumenda numquam, repudiandae rem laboriosam amet nemo temporibus! Velit cumque exercitationem excepturi accusamus veritatis! Corporis eveniet neque nemo eos. Beatae hic ex expedita repellendus tenetur nobis ad vitae, dicta quibusdam sed dolorum, modi natus?</p><br />
+    </div>
   )
 }
