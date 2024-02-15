38de15b7a40d93db73dfbf9218d4b5e6c2525106
Abhishek Kumar Singh <singh.abhishek151019@gmail.com>
2024-02-15 13:48:36 +0530

    updated

--- src/admin/Forms/AdminCategory.jsx
+++ src/admin/Forms/AdminCategory.jsx
@@ -1,39 +1,36 @@
 import React from 'react'
-
-// FORM
-
-// Category Name
-// Description
-// Image
-// Active/Inactive
+import CategoriesTable from '../Tables/CategoriesTable'
 
 export default function AdminCategory() {
   return (
-    <div className='admincategory'>
-      <h2 className='categoryh2'>Product Category</h2>
-      <hr className='hrankit'/>
-      <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">Category</span>
-        <input type="text" class="form-control" placeholder="Category Name..." aria-label="Username" aria-describedby="basic-addon1"/>
+    <div className='d-flex w-100'>
+      <div className='admincategory'>
+        <h2 className='categoryh2'>Product Category</h2>
+        <hr className='hrankit' />
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="basic-addon1">Category</span>
+          <input type="text" class="form-control" placeholder="Category Name..." aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
+        <br />
+        <div class="mb-3">
+          <label for="formFileSm" class="form-label">Product Image :</label>
+          <input class="form-control form-control-sm" id="formFileSm" type="file" />
+        </div>
+        <br />
+        <div class="form-floating">
+          <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+          <label for="floatingTextarea">Product Description</label>
+        </div>
+        <br />
+        <h5>Category Status</h5>
+        <div class="form-check form-switch ">
+          <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+          <label class="form-check-label" for="flexSwitchCheckDefault"> Active / Inactive</label>
+        </div>
+        <br />
+        <button type="button" class="btn btn-outline-primary">Submit</button>
       </div>
-      <br />
-      <div class="mb-3">
-        <label for="formFileSm" class="form-label">Product Image :</label>
-        <input class="form-control form-control-sm" id="formFileSm" type="file"/>
-      </div>
-      <br />
-      <div class="form-floating">
-        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-        <label for="floatingTextarea">Product Description</label>
-      </div>
-      <br />
-      <h5>Category Status</h5>
-      <div class="form-check form-switch ">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-        <label class="form-check-label" for="flexSwitchCheckDefault"> Active / Inactive</label>
-      </div>
-      <br />
-      <button type="button" class="btn btn-outline-primary">Submit</button>
+      <CategoriesTable />
     </div>
   )
 }
---
+++ src/admin/Tables/CategoriesTable.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+export default function CategoriesTable() {
+  return (
+    <div className='body' >
+      <main className="table">
+        <section className="table__header">
+          <h1>User Details</h1>
+        </section>
+        <section className="table__body">
+          <table>
+            <thead>
+              <tr>
+                <th>Profile Picture</th>
+                <th>First Name</th>
+                <th>Phone Number</th>
+                <th>Email Address</th>
+                <th>City</th>
+                <th>Postal/ZIP Code</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><img src='' alt="profile picture" /></td>
+                <td>Ankit</td>
+                <td>9340438468</td>
+                <td>ankitkumar@gmail.com</td>
+                <td>Durg</td>
+                <td>XYZ50</td>
+              </tr>
+              <tr>
+                <td><img src='' alt="profile picture" /></td>
+                <td>Ankit</td>
+                <td>9340438468</td>
+                <td>ankitkumar@gmail.com</td>
+                <td>Durg</td>
+                <td>XYZ50</td>
+              </tr>
+              <tr>
+                <td><img src='' alt="profile picture" /></td>
+                <td>Ankit</td>
+                <td>9340438468</td>
+                <td>ankitkumar@gmail.com</td>
+                <td>Durg</td>
+                <td>XYZ50</td>
+              </tr>
+              <tr>
+                <td><img src='' alt="profile picture" /></td>
+                <td>Ankit</td>
+                <td>9340438468</td>
+                <td>ankitkumar@gmail.com</td>
+                <td>Durg</td>
+                <td>XYZ50</td>
+              </tr>
+              <tr>
+                <td><img src='' alt="profile picture" /></td>
+                <td>Ankit</td>
+                <td>9340438468</td>
+                <td>ankitkumar@gmail.com</td>
+                <td>Durg</td>
+                <td>XYZ50</td>
+              </tr>
+              <tr>
+                <td><img src='' alt="profile picture" /></td>
+                <td>Ankit</td>
+                <td>9340438468</td>
+                <td>ankitkumar@gmail.com</td>
+                <td>Durg</td>
+                <td>XYZ50</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </main>
+    </div>
+  )
+}
