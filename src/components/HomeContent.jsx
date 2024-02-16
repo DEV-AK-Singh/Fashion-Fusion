ee3334d211a1fe462b602ffbe3f1837f315ba376
John Austin <johnaustin2799@gmail.com>
2024-02-16 22:19:13 +0530

    home-category done

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -8,26 +8,26 @@ export default function HomeContent() {
     <div>
         <h1 className='cs-text-primary text-center my-lg-5 my-2 display-5'>Shop By Category</h1>
         <div className='container-fluid px-lg-5 row p-0 m-0 d-flex justify-content-center'>
-            <CategoryCard link={'/category/oversized-tee'} title={'Oversized Tee'} sale={"30-40%"} imgUrl={"https://cdn.shopify.com/s/files/1/1746/5485/products/1_568d0ae6-c5ca-412c-a266-25abedcad655_540x.jpg?v=1663073700"}/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
+            <CategoryCard link={'/category/Women Printed Crepe A-line Kurta  (Grey)'} title={'Women Printed Crepe A-line Kurta  (Grey)'} sale={"30-40%"} imgUrl={"https://rukminim2.flixcart.com/image/832/832/xif0q/shopsy-kurta/q/g/s/m-shopsy-round-ku-256-dsk-studio-original-imags9v6q99yvm6b.jpeg?q=70&crop=true"}/>
+            <CategoryCard link={'/category/Women Printed Crepe A-line Kurta  (Pink)'} title={'Women Printed Crepe A-line Kurta  (Pink)'} sale={'35-45%'} imgUrl={"https://rukminim2.flixcart.com/image/832/832/xif0q/kurta/q/m/v/s-round-ku-255-dsk-studio-original-imagsarncgex3um2.jpeg?q=70&crop=true"}/>
+            <CategoryCard link={'/category/Women Printed Crepe A-line Kurta (White)'} title={'Women Printed Crepe A-line Kurta (White)'} sale={'72%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/kurta/b/a/t/xxl-round-ku-252-dsk-studio-original-imagsarn4hmhncxf.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Women Printed Crepe A-line Kurta  (Grey)'} title={'Women Printed Crepe A-line Kurta  (Grey)'} sale={'68%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/kurta/e/8/c/m-round-ku-256-dsk-studio-original-imagsarn7g6rehyz.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Women Printed Crepe Straight Kurta'} title={'Women Printed Crepe Straight Kurta '} sale={'62%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/kurta/0/w/y/m-button-kurti-232-dsk-studio-original-imagrhcqwdha7pdh.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Women Printed Crepe Straight Kurta (Blue)'} title={'Women Printed Crepe Straight Kurta  (Blue)'} sale={'62%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/kurta/b/3/h/m-button-kurti-240-dsk-studio-original-imagrhcpfq4y9tqh.jpeg?q=70&crop=true'}/>
             <br />
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
+            <CategoryCard link={'/category/Men Regular Fit Solid Spread Collar Formal Shirt(Black)'} title={'Men Regular Fit Solid Spread Collar Formal Shirt(Black)'} sale={'50%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/f/i/9/l-epb1-eviqe-original-imagt3musxgc3qgg.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Men Regular Fit Solid Spread Collar Formal Shirt(Maroon)'} title={'Men Regular Fit Solid Spread Collar Formal Shirt(Maroon)'} sale={'62%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/b/d/f/3xl-13-lstr-wine-vtexx-original-imagnzbummhkgr7p.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Men Slim Fit Solid Spread Collar Formal Shirt(White)'} title={'Men Slim Fit Solid Spread Collar Formal Shirt(White)'} sale={'72%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/k/z/l/l-epb1-eviqe-original-imagtaceg9tpytc9.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Men Regular Fit Solid Spread Collar Formal Shirt(Blue)'} title={'Men Regular Fit Solid Spread Collar Formal Shirt(Blue)'} sale={'80%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/1/5/g/m-mnbv-st02-matromaut-original-imagxazang47hazx.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Men Regular Fit Solid Spread Collar Formal Shirt(pink)'} title={'Men Regular Fit Solid Spread Collar Formal Shirt(pink)'} sale={'72%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/g/c/i/s-mnbv-st07-matromaut-original-imagxaz9frrchhvk.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Men Regular Fit Solid Spread Collar Formal Shirt(Orange)'} title={'Men Regular Fit Solid Spread Collar Formal Shirt(Orange)'} sale={'65%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/d/w/b/s-13-lstr-peach-vtexx-original-imagxtnyc2c4fgdu.jpeg?q=70&crop=true'}/>
             <br />
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
+            <CategoryCard link={'/category/Casual Lifestyle Shoes for Men | Soft Cushioned Insole, Slip-Resistance Sneakers For Men  (White)'} title={'Casual Lifestyle Shoes for Men | Soft Cushioned Insole, Slip-Resistance Sneakers For Men  (White)'} sale={'80%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/k/h/v/7-rsl021-red-tape-white-original-imagxrn4h2yqrten.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/KL20050 Lightweight Comfort Summer Trendy Premium Stylish Derby For Men  (Brown)'} title={'KL20050 Lightweight Comfort Summer Trendy Premium Stylish Derby For Men  (Brown)'} sale={'72%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/8/w/3/6-kl20050-brown-6-killer-brown-original-imaggsgqtaze2kjy.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/MIKE (N) Running Shoes For Men  (Blue)'} title={'MIKE (N) Running Shoes For Men  (Blue)'} sale={'60%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/xif0q/shoe/d/o/e/9-5g-845-campus-mod-blu-wht-bg-enhanced-original-imag4g3xnanhz7zk-bb.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Mens Mid-top Comfort Uniform Casual Padded Cushioned Army Uniform Style Shoes Boots For Men  (Tan)'} title={'Mens Mid-top Comfort Uniform Casual Padded Cushioned Army Uniform Style Shoes Boots For Men  (Tan)'} sale={'50%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/7/f/v/5-fc-35-tan-5-field-care-tan-original-imagsv9dabfmtnwq.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Genuine Leather Ankle Length Boots for Men | Soft Cushioned Insole Boots For Men  (Black)'} title={'Genuine Leather Ankle Length Boots for Men | Soft Cushioned Insole Boots For Men  (Black)'} sale={'75%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/f/q/9/9-rte3191c-9-red-tape-black-original-imagu5hszjdwrrfh.jpeg?q=70&crop=true'}/>
+            <CategoryCard link={'/category/Genuine Leather Ankle Length Boots for Men | Soft Cushioned Insole Boots For Men  (Tan)'} title={'Genuine Leather Ankle Length Boots for Men | Soft Cushioned Insole Boots For Men  (Tan)'} sale={'75%'} imgUrl={'https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/x/n/8/7-rte3197c-7-red-tape-tan-original-imaguynwzufbmw7y.jpeg?q=70&crop=true'}/>
         </div>
     </div>
   )
