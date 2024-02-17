8e1911eb7312970ddfa3c40bfc27645688573def
John Austin <johnaustin2799@gmail.com>
2024-02-17 12:04:09 +0530

    FAQ done

--- src/pages/FAQs.jsx
+++ src/pages/FAQs.jsx
@@ -2,6 +2,72 @@ import React from 'react'
 
 export default function FAQs() {
   return (
-    <div>FAQs</div>
+    <div>
+       <div className="container border shadow mb-5 mt-3">
+    <h1 className='text-decoration-underline mb-4 '>FAQs</h1>
+    
+    <div class="faq-item">
+      <div class="question">How quickly can I get my order delivered?</div>
+      <div class="answer">
+        <p>Orders will be delivered by the date you see on the product page for your location.</p>
+      </div>
+    </div>
+    
+    <div class="faq-item">
+      <div class="question">Can I reactivate my inactive account?</div>
+      <div class="answer">
+        <p>Yes. To reactivate your Flipkart account, simply log in with your registered email ID or mobile number/password combination used before deactivation. Your account data will be fully restored, default subscription settings applied.</p>
+      </div>
+    </div>
+    
+    <div class="faq-item">
+      <div class="question">How do I return my order ?</div>
+      <div class="answer">
+        <p>Place a return request in the Orders page. You will get an option to choose refund/replace/exchange as per our return policy.</p>
+      </div>
+    </div>
+    
+    <div class="faq-item">
+      <div class="question">What is the return policy for cash on delivery?</div>
+      <div class="answer">
+        <p>In case of cash on delivery,you will have to provide a bank account number for the refund.For replacement/exchange,you will be given an alternate product for the returned product.</p>
+      </div>
+    </div>
+
+    <div class="faq-item">
+      <div class="question">What if I miss my order pickup?</div>
+      <div class="answer">
+        <p>Do not worry,the delivery agent will try to pick up again on the next working day.</p>
+      </div>
+    </div>
+
+    <div class="faq-item">
+      <div class="question">How does Flipkart prevent card fraud?</div>
+      <div class="answer">
+        <p>Online payments are monitored by our systems for any suspicious activity and some transactions are verified through extensive checks if we find that they are not authorised by the owner of the card. When we're not able to rule fraud out in rare cases, the transaction is kept on hold and we ask the shopper to share relevant proofs. This is done to make sure that the transaction is genuine and authorised.</p>
+      </div>
+    </div>
+
+    <div class="faq-item">
+      <div class="question">What is an OTP or verification code?</div>
+      <div class="answer">
+        <p>An OTP (One Time Password) or verification code is a password/code used to verify your mobile number or email ID when you log into your Flipkart account for the first time.
+
+OTP verification will be also be done in other cases like creating an account, password recovery, and updating email/mobile details.</p>
+      </div>
+    </div>
+
+    <div class="faq-item">
+      <div class="question">How long does it take to cancel an order?</div>
+      <div class="answer">
+        <p>Cancellation of item(s) in an order happens immediately if the order hasn't been shipped yet by the seller. If your order has been shipped, it will be cancelled as soon as the courier service confirms that the shipment is being returned to the seller. Orders from certain categories cannot be cancelled after 24 hours, please check the product page for more details.</p>
+      </div>
+    </div>
+
+
+
+
+  </div>
+    </div>
   )
 }
