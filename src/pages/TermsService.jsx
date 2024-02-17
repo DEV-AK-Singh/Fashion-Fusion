86ec0d6cf998ea7261d2edd012213e169d0b235d
John Austin <johnaustin2799@gmail.com>
2024-02-17 11:36:45 +0530

    term and services done

--- src/pages/TermsService.jsx
+++ src/pages/TermsService.jsx
@@ -2,6 +2,34 @@ import React from 'react'
 
 export default function TermsService() {
   return (
-    <div>TermsService</div>
+    <div>
+       <div className="container border w-75 border border-1 mb-5 mt-5 shadow ">
+    <h1 id='x'>Terms and Conditions</h1>
+    <p>Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern faishon fusion's relationship with you in relation to this website. If you disagree with any part of these terms and conditions, please do not use our website.</p>
+    
+    <h2>Content</h2>
+    <ol>
+      <li>The content of the pages of this website is for your general information and use only. It is subject to change without notice.</li>
+      <li>This website uses cookies to monitor browsing preferences. If you do allow cookies to be used, the following personal information may be stored by us for use by third parties: [list of information].</li>
+    </ol>
+    
+    <h2>License</h2>
+    <ol>
+      <li>This website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.</li>
+      <li>All trademarks reproduced in this website, which are not the property of, or licensed to the operator, are acknowledged on the website.</li>
+    </ol>
+    
+    <h2>Links</h2>
+    <ol>
+      <li>From time to time, this website may also include links to other websites. These links are provided for your convenience to provide further information. They do not signify that we endorse the website(s). We have no responsibility for the content of the linked website(s).</li>
+    </ol>
+    
+    <h2>Disclaimer</h2>
+    <ol>
+      <li>Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.</li>
+      <li>This website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance, and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.</li>
+    </ol>
+  </div>
+    </div>
   )
 }
